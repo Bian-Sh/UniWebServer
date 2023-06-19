8784819f3df21fac437b8511899d3efebeafc3ca
雨落随风 <bshsf@qq.com>
2023-06-20 02:57:30 +0800

    [Add] 想把上传页面改成支持动态多项文件上传
    已知问题：
    1. 可能 fallback to index html 逻辑有问题，upload 和 upload/ 表现不一致，待确认
    2. html 需要优化，需要有能力接受server 返回的上传成功后的文件信息并布局到上传界面

--- Assets/Samples/Uni Web Server/0.1.0/FileUploadSample/Scripts/FileUploadSample.cs
+++ Assets/Samples/Uni Web Server/0.1.0/FileUploadSample/Scripts/FileUploadSample.cs
@@ -83,5 +83,73 @@ namespace zFramework.Web.Samples
             return html;
         }
 
+        [Route("/upload")]
+        public async Task<string> FileUploadPageV2(HttpListenerRequest request)
+        {
+            Debug.Log($"{nameof(FileUploadSample)}: file upload v2 is called {request.HttpMethod}");
+            var uploadedFileHtml = "";
+            if (request.HttpMethod == "POST")
+            {
+                Debug.Log($"{nameof(FileUploadSample)}: 0. Thread ID = {Thread.CurrentThread.ManagedThreadId}");
+                await ToOtherThread;// 使用其他线程做文件解析
+                Debug.Log($"{nameof(FileUploadSample)}: 1. Thread ID = {Thread.CurrentThread.ManagedThreadId}");
+                var parser = await MultipartFormDataParser.ParseAsync(request.InputStream);
+
+                if (parser.Files.Count >= 1)
+                {
+                    //  把获取到的文件保存到 StreamingAssets 文件夹下 
+                    async Task SaveFileAsync(FilePart file)
+                    {
+                        var filename = Uri.UnescapeDataString(file.FileName);
+                        var path = Path.Combine(Application.streamingAssetsPath, savePath, filename);
+                        var folder = Path.GetDirectoryName(path);
+                        if (!Directory.Exists(folder))
+                        {
+                            Directory.CreateDirectory(folder);
+                        }
+                        using (var fileStream = new FileStream(path, FileMode.Create))
+                        {
+                            Debug.Log($"{nameof(FileUploadSample)}: 2. Thread ID = {Thread.CurrentThread.ManagedThreadId}");
+                            await ToOtherThread;
+                            await file.Data.CopyToAsync(fileStream);
+                        }
+                    }
+
+                    foreach (var item in parser.Files)
+                    {
+                        uploadedFileHtml +=
+                            $@"<div>
+                             <p>FileName: {item.FileName}</p>
+                             <p>ContentType: {item.ContentType}</p>
+                             <p>FileSize: {item.Data.Length} bytes</p>
+                           </div>" + Environment.NewLine;
+                        _ = SaveFileAsync(item);
+                    }
+
+                    await ToMainThread;
+                    var file = parser.Files[0];
+                    if (file.ContentType.StartsWith("image/"))
+                    {
+                        _ = imageViewer.ShowImageAsync(file.Data);
+                    }
+                }
+            }
+
+            var html =
+                $@"<html>
+                     <body>
+                       {uploadedFileHtml}
+                       <form method = ""post"" enctype=""multipart/form-data"">
+                         <input type=""file"" name=""file""/>
+                         <input type=""file"" name=""file""/>
+                         <input type=""file"" name=""file""/>
+                         <br/>
+                         <br/>
+                         <input type=""submit"" value=""send""/>
+                       </form>
+                    </body>
+                  </html>";
+            return html;
+        }
     }
 }
